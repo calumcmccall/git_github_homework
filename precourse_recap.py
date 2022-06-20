6441db02b781087a9bc9bcffec84c8628f9e0eff
Calum <calumcmccall@gmail.com>
2022-06-20 21:42:56 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,31 @@
+word_1 = "A"
+word_2 = "small"
+word_3 = "tale"
+
+def sentence(first_word, second_word, third_word):
+    sentence = first_word + " " + second_word + " " + third_word
+    print(sentence)
+
+sentence(word_1, word_2, word_3)
+
+def uppercase(word):
+    uppercase_word = word.upper
+    print(uppercase_word)
+
+uppercase(word_1)
+
+first_number = 18
+second_number = 36
+third_number = 92
+
+def total(first_number, second_number, third_number):
+    total = first_number + second_number + third_number
+    print(total)
+
+total(first_number, second_number, third_number)
+
+def multiply_divide(first_number, second_number, third_number):
+    multiply_divide = first_number * second_number / third_number
+    print(multiply_divide)
+
+multiply_divide(first_number, second_number, third_number)
